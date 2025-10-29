96b73e129d06c13fb9c42ad48f2f73e8d3951043
bukinys <bukinys@yandex.ru>
2025-10-29 12:00:04 +0800

    taxonomic diversity
    Script for the R programming language for analyzing taxonomic diversity using 16S and 18S metabarcoding data

---
+++ R_taxonomic_diversity_analysis.r
@@ -0,0 +1,27 @@
+
+library(vegan)
+
+setwd("G:\\Data_2022-08-14\\Metagenomic_analysis\\Анализ_ампликонов_Михайлов\\Сезонная_динамика_new\\Скрипты_данные_для_Githab\\Таксономичское_разнообразие")
+
+#reading the genotype counte table
+data<-read.csv("ASV_16S.txt", header=TRUE, sep="\t", check.names = FALSE)
+
+data_count<-data[, -ncol(data)]
+rownames(data_count)<-data_count[,1]
+data_count<-data_count[,-1]
+
+d_f<-as.data.frame(t(data_count))
+
+#plotting rarefaction curves
+rc<-rarecurve(d_f, step = 20, sample=70, col = "blue", cex=0.6)
+
+
+#calculation of the expected number of genotypes - Chao1 and ASE, calculation of the Shannon and Simpson index
+diversiti<-t(estimateR(d_f))
+shannon<-diversity(d_f, index = "shannon")
+simpson<-diversity(d_f, index = "simpson")
+diversiti<-cbind(sample_id=rownames(diversiti), reads_number=rowSums(d_f),diversiti,  shannon=shannon, simpson=simpson)
+diversiti<-as.data.frame(diversiti)
+
+diversiti #visualization of a table with taxonomic diversity indicators
+
